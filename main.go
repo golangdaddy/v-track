cd972057283755822df4ba775a9570c0faf53b6b
AlexBreadman <alex.b@xiatech.co.uk>
2021-09-14 22:11:48 +0100

    poc

--- main.go
+++ main.go
@@ -3,32 +3,95 @@ package main
 import (
     "os"
     "fmt"
+    "time"
     "bytes"
     "strings"
 
-    "encoding/hex"
+    "io/fs"
+    "path/filepath"
+    "encoding/json"
 
     "github.com/fatih/color"
 	"tinygo.org/x/bluetooth"
 )
 
-var adapter = bluetooth.DefaultAdapter
+type StoredDevice struct {
+    UID string
+    LastSeen time.Time
+    CustomName string
+    Seen int
+}
 
-func (app *App) markSeen(uid string) {
-    addr := fmt.Sprintf(
-        "%s:%s:%s",
-        string(uid[:2]),
-        string(uid[2:4]),
-        string(uid[4:6]),
-    )
-    println("Finding", addr)
-    descr, ok := app.db[addr]
-    if ok {
-        for _, d := range descr {
-            color.Green(d)
+type App struct {
+    adapter *bluetooth.Adapter
+    discovered chan bluetooth.ScanResult
+    db map[string][]string
+    seen map[string]*StoredDevice
+    notSeen map[string]*StoredDevice
+}
+
+func (app *App) logic() {
+    for {
+
+        select {
+        case device := <- app.discovered:
+
+            uid := device.Address.String()
+
+            println("found device:", uid, device.RSSI, device.LocalName(), device.Address.String())
+
+            sd := &StoredDevice{
+                UID: uid,
+                LastSeen: time.Now().UTC(),
+            }
+
+
+            b, err := os.ReadFile("./seen/"+uid)
+            if err == nil {
+                // open existing file
+                err := json.Unmarshal(b, sd)
+                if err != nil {
+                    panic(err)
+                }
+                sd.Seen++
+            }
+
+            delete(app.notSeen, uid)
+            app.seen[uid] = sd
+
+            // flush to file
+            b, err = json.Marshal(sd)
+            if err != nil {
+                panic(err)
+            }
+            if err := os.WriteFile("seen/"+uid, b, 0666); err != nil {
+                panic(err)
+            }
+
+        //case <- time.NewTicker(time.Second).C: println("ticker")
+
         }
     }
-    app.seen[uid] = true
+}
+
+func (app *App) checkSeen() {
+    for {
+        time.Sleep(5 * time.Second)
+        for k, sd := range app.seen {
+            if sd.Seen != 0 {
+                color.Green("SEEN %s %v", k, sd)
+            } else {
+                color.Yellow("SEEN %s %v", k, sd)
+
+            }
+        }
+        color.Red("%d Total AWOL", len(app.notSeen))
+        for k, sd := range app.notSeen {
+            if sd.Seen > 0 {
+                color.Red("NOT SEEN %s %v", k, sd)
+            }
+        }
+    }
 }
 
 func (app *App) loadManufacturers() {
@@ -69,78 +132,66 @@ func (app *App) loadManufacturers() {
     }
 }
 
-type App struct {
-    db map[string][]string
-    seen map[string]bool
+func (app *App) loadSeen() {
+    if err := filepath.WalkDir(
+        "./seen",
+        func(path string, info fs.DirEntry, err error) error {
+            println(path)
+            if path == "./seen" {
+                return nil
+            }
+            b, err := os.ReadFile(path)
+            if err != nil {
+                return err
+            }
+            sd := &StoredDevice{}
+            if err := json.Unmarshal(b, sd); err != nil {
+                return err
+            }
+            uid := strings.Split(path, "/")[1]
+            app.notSeen[uid] = sd
+            delete(app.seen, uid)
+            return nil
+        },
+    ); err != nil {
+        panic(err)
+    }
 }
 
 func main() {
 
+    os.Mkdir("seen", 0666)
+
     app := &App{
+        adapter: bluetooth.DefaultAdapter,
+        discovered: make(chan bluetooth.ScanResult, 10),
         db: map[string][]string{},
-        seen: map[string]bool{},
+        seen: map[string]*StoredDevice{},
+        notSeen: map[string]*StoredDevice{},
     }
-    app.loadManufacturers()
+    //app.loadManufacturers()
+    go app.checkSeen()
+    go app.logic()
 
 	// Enable BLE interface.
-	must("enable BLE stack", adapter.Enable())
+	if err := app.adapter.Enable(); err != nil {
+        panic(err)
+    }
 
-	// Start scanning.
-	println("scanning...")
+    for {
+    	// Start scanning.
+        app.loadSeen()
 
-	err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
-        uid, err := Parse(device.Address.String())
-        if err != nil {
+        go func () {
+            time.Sleep(2 * time.Minute)
+            app.adapter.StopScan()
+        }()
+    	println("scanning...")
+    	if err := app.adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
+            app.discovered <- device
+    	}); err != nil {
             panic(err)
         }
-        uuid := fmt.Sprintf("%X", uid)
-		println("found device:", uuid, len(uid), device.RSSI, device.LocalName(), device.Address.String())
-        app.markSeen(uuid)
-	})
-	must("start scan", err)
-}
-
-func must(action string, err error) {
-	if err != nil {
-		panic("failed to " + action + ": " + err.Error())
-	}
-}
-
-type UUID []byte
-
-// Parse parses a standard-format UUID string, such
-// as "1800" or "34DA3AD1-7110-41A1-B1EF-4430F509CDE7".
-func Parse(s string) (UUID, error) {
-	s = strings.Replace(s, "-", "", -1)
-	b, err := hex.DecodeString(s)
-	if err != nil {
-		return nil, err
-	}
-	if err := lenErr(len(b)); err != nil {
-		return nil, err
-	}
-	return UUID(Reverse(b)), nil
-}
-
-// lenErr returns an error if n is an invalid UUID length.
-func lenErr(n int) error {
-	switch n {
-	case 2, 16:
-		return nil
-	}
-	return fmt.Errorf("UUIDs must have length 2 or 16, got %d", n)
-}
-
-// Reverse returns a reversed copy of u.
-func Reverse(u []byte) []byte {
-	// Special-case 16 bit UUIDS for speed.
-	l := len(u)
-	if l == 2 {
-		return []byte{u[1], u[0]}
-	}
-	b := make([]byte, l)
-	for i := 0; i < l/2+1; i++ {
-		b[i], b[l-i-1] = u[l-i-1], u[i]
-	}
-	return b
+        println("clean scan exit")
+    }
 }
