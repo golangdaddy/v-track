282e4e7d004be4724723f56ae909db78590eb468
AlexBreadman <alex.b@xiatech.co.uk>
2021-09-16 18:56:55 +0100

    fix logic

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
     "os"
     "fmt"
+    "sync"
     "time"
+    "sort"
     "bytes"
     "strings"
 
@@ -18,6 +20,7 @@ import (
 type StoredDevice struct {
     UID string
     LastSeen time.Time
+    LocalName string
     CustomName string
     Seen int
 }
@@ -28,23 +31,47 @@ type App struct {
     db map[string][]string
     seen map[string]*StoredDevice
     notSeen map[string]*StoredDevice
+    sync.RWMutex
+}
+
+func (app *App) see(uid string, sd *StoredDevice) {
+    app.Lock()
+    defer app.Unlock()
+    delete(app.notSeen, uid)
+    app.seen[uid] = sd
+}
+
+func (app *App) unsee(uid string, sd *StoredDevice) {
+    app.Lock()
+    defer app.Unlock()
+    delete(app.seen, uid)
+    app.notSeen[uid] = sd
 }
 
 func (app *App) logic() {
     for {
-
         select {
         case device := <- app.discovered:
 
             uid := device.Address.String()
 
-            println("found device:", uid, device.RSSI, device.LocalName(), device.Address.String())
-
             sd := &StoredDevice{
                 UID: uid,
-                LastSeen: time.Now().UTC(),
+                CustomName: "?",
+                LocalName: device.LocalName(),
             }
 
+            if len(sd.LocalName) > 0 {
+                println("skip device:", uid, device.RSSI, device.LocalName(), device.Address.String())
+                b, err := json.Marshal(sd)
+                if err != nil {
+                    panic(err)
+                }
+                if err := os.WriteFile("./skipped/"+uid, b, 0666); err != nil {
+                    panic(err)
+                }
+                continue
+            }
 
             b, err := os.ReadFile("./seen/"+uid)
             if err == nil {
@@ -54,10 +81,18 @@ func (app *App) logic() {
                     panic(err)
                 }
                 sd.Seen++
+            } else {
+                println("found device:", uid, device.RSSI, device.LocalName(), device.Address.String())
             }
 
-            delete(app.notSeen, uid)
-            app.seen[uid] = sd
+            // set correct time on the record
+            sd.LastSeen = time.Now().UTC()
+            d := time.Since(sd.LastSeen)
+            if d > (10 * time.Minute) {
+                println("THEY HAVE COME BACK TO US", d.String())
+            }
+
+            app.see(uid, sd)
 
             // flush to file
             b, err = json.Marshal(sd)
@@ -74,23 +109,44 @@ func (app *App) logic() {
     }
 }
 
+type DeviceTuple struct {
+    K string
+    V *StoredDevice
+}
+
 func (app *App) checkSeen() {
     for {
         time.Sleep(5 * time.Second)
+        app.RLock()
+        list := []*DeviceTuple{}
         for k, sd := range app.seen {
             if sd.Seen != 0 {
                 color.Green("SEEN %s %v", k, sd)
             } else {
                 color.Yellow("SEEN %s %v", k, sd)
-
             }
+            list = append(
+                list,
+                &DeviceTuple{
+                    K: k,
+                    V: sd,
+                },
+            )
         }
+        sort.Slice(
+            list,
+            func (j, k int) bool {
+                return list[k].V.LastSeen.After(list[j].V.LastSeen)
+            },
+        )
+
         color.Red("%d Total AWOL", len(app.notSeen))
         for k, sd := range app.notSeen {
-            if sd.Seen > 0 {
+            if sd.Seen > 1 && (time.Since(sd.LastSeen) > (3 * time.Minute)) {
                 color.Red("NOT SEEN %s %v", k, sd)
             }
         }
+        app.RUnlock()
     }
 }
 
@@ -132,7 +188,7 @@ func (app *App) loadManufacturers() {
     }
 }
 
-func (app *App) loadSeen() {
+func (app *App) loadSeen(m map[string]struct{}) {
     if err := filepath.WalkDir(
         "./seen",
         func(path string, info fs.DirEntry, err error) error {
@@ -149,8 +205,15 @@ func (app *App) loadSeen() {
                 return err
             }
             uid := strings.Split(path, "/")[1]
-            app.notSeen[uid] = sd
-            delete(app.seen, uid)
+            if _, ok := m[uid]; ok {
+                return nil
+            }
+
+            if time.Since(sd.LastSeen) > (2 * time.Minute) {
+                app.unsee(uid, sd)
+            } else {
+                app.see(uid, sd)
+            }
             return nil
         },
     ); err != nil {
@@ -160,7 +223,8 @@ func (app *App) loadSeen() {
 
 func main() {
 
-    os.Mkdir("seen", 0666)
+    os.Mkdir("seen", 0777)
+    os.Mkdir("skipped", 0777)
 
     app := &App{
         adapter: bluetooth.DefaultAdapter,
@@ -179,15 +243,23 @@ func main() {
     }
 
     for {
+        m := map[string]struct{}{}
+
     	// Start scanning.
-        app.loadSeen()
+        app.loadSeen(m)
 
         go func () {
-            time.Sleep(2 * time.Minute)
+            time.Sleep(time.Minute)
             app.adapter.StopScan()
         }()
     	println("scanning...")
     	if err := app.adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
+            uid := device.Address.String()
+            if _, ok := m[uid]; ok {
+                println("skip scan exit")
+                return
+            }
+            m[uid] = struct{}{}
             app.discovered <- device
     	}); err != nil {
             panic(err)
